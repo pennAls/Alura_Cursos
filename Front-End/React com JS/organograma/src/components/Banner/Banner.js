fea05995d904db566c7a8eb83ad6a1d6485afc59
Lucas Pena Araújo dos Santos <143518683+pennAls@users.noreply.github.com>
2024-12-02 20:07:22 -0300

    Feature Banner

---
+++ Front-End/React com JS/organograma/src/components/Banner/Banner.js
@@ -0,0 +1,11 @@
+import "./Banner.css";
+
+function Banner() {
+  return (
+    <header className = "banner">
+      <img src="/imgs/banner.png" alt="banner_principal_lp" />;
+    </header>
+  );
+}
+
+export default Banner;
