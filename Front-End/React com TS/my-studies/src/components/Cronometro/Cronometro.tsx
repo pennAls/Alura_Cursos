9c9c3fd5dce051ebec5f1315a8e2a82863d4b65b
pennA <lucaspsantos003@gmail.com>
2025-02-09 23:23:05 -0300

    Cronometro 2/3

--- Front-End/React com TS/my-studies/src/components/Cronometro/Cronometro.tsx
+++ Front-End/React com TS/my-studies/src/components/Cronometro/Cronometro.tsx
@@ -1,29 +1,28 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { TarefaContext } from "../../App";
-import Botao from "../Botao"
-import Relogio from "./Relogio"
-import style from  "./cronometro.module.scss"
+import Botao from "../Botao";
+import Relogio from "./Relogio";
+import style from "./cronometro.module.scss";
 import { timeToSeconds } from "../../common/utils/time";
+import { Itarefa } from "../../types/Itarefa";
 
+export const Cronometro = () => {
+  const { selectedTarefa } = useContext(TarefaContext);
+  const tempo = (selectedTarefa as Itarefa)?.tempo ?? "00:00:00";
+  const [tempoState, setTempo] = useState<number>();
 
-export const Cronometro = () =>{
-    const { selectedTarefa } = useContext(TarefaContext);
-    const tarefa = { ...selectedTarefa };
-    const tempo = tarefa?.tempo ?? "00:00:00";
-    const [tempoState,setTempo] = useState<Number>();
-    setTempo(timeToSeconds(tempo))
+  useEffect(() => {
+    setTempo(timeToSeconds(tempo));
+  }, [selectedTarefa, tempo]);
 
-    
-
-    return(
-        <div className= {style.cronometro}>
-            <p className= {style.titulo}>Escolha um Card e inicie o cronômetro</p>
-            <div className= {style.relogioWrapper}>
-            <Relogio/>
-            </div>
-            <Botao>
-                Começar
-            </Botao>
-        </div>
-    )
-}
+  return (
+    <div className={style.cronometro}>
+      Tempo:{tempoState}
+      <p className={style.titulo}>Escolha um Card e inicie o cronômetro</p>
+      <div className={style.relogioWrapper}>
+        <Relogio />
+      </div>
+      <Botao>Começar</Botao>
+    </div>
+  );
+};
