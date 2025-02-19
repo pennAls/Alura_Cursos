af9c783867e550a673bb734102eb36ab878a36cb
pennA <lucaspsantos003@gmail.com>
2025-02-19 11:44:14 -0300

    Att de arquivos

--- Front-End/React com TS/my-studies/src/components/Lista/index.tsx
+++ Front-End/React com TS/my-studies/src/components/Lista/index.tsx
@@ -1,2 +1,18 @@
-import {Lista} from "./Lista"
-export default Lista
+import { Itarefa } from "../../types/Itarefa";
+import Item from "./Item";
+import style from "./Lista.module.scss";
+
+const Lista = ({ tarefas }: { tarefas: Itarefa[] }) => {
+  return (
+    <aside className={style.listaTarefas}>
+      <h2>Estudos do Dia</h2>
+      <ul>
+        {tarefas.map((item, index) => (
+          <Item key={index} {...item}></Item>
+        ))}
+      </ul>
+    </aside>
+  );
+};
+
+export default Lista;
